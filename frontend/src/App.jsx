61129e87f1ac88c6f1b2f432de2dd81bb403c14e
Thibault BENOIT <thibaultbenoit0@gmail.com>
2023-06-05 15:06:33 +0200

    correction architecture commune

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,16 @@
 import "./App.css";
 import "./index.scss";
+import Footer from "./components/footer/Footer";
+import Header from "./components/header/Header";
 
 function App() {
-  return <div className="App" />;
+  return (
+    <div className="App">
+      <Header />
+      <main />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
