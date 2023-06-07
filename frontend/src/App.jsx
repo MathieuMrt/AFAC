755b4cfab68136f4687646151b42940a1cdaa697
Thibault BENOIT <thibaultbenoit0@gmail.com>
2023-06-07 09:49:13 +0200

    correction es-lint après conflit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,16 +3,14 @@ import "./index.scss";
 import Router from "./router/Router";
 import Footer from "./components/footer/Footer";
 import Header from "./components/header/Header";
-import Accueil from "./components/accueil/Accueil";
 
 function App() {
   return (
     <div className="App">
-
       <Header />
-        <Header />
-        <Router />;
-        <main />
+      <Header />
+      <Router />;
+      <main />
       <Footer />
     </div>
   );
