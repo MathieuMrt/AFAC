c9bb7dda3a6d4a19fe65331bdf19ca5861696d10
Mathieu Mrt <martinmathieu49@gmail.com>
2023-06-07 11:45:24 +0200

    AF-8 Ajout du contenu au composant description

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,16 +3,17 @@ import "./index.scss";
 import Router from "./router/Router";
 import Footer from "./components/footer/Footer";
 import Header from "./components/header/Header";
+import Description from "./components/description/Description";
 
 function App() {
   return (
     <div className="App">
-      <div className="header_main">
-        <Header />
-        <Router />;
-        <main />
-      </div>
-      <Footer />
+      <Header />
+      <Router />
+      <main className="app_main">
+        <Description />
+        <Footer />
+      </main>
     </div>
   );
 }
