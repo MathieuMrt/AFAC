95d32c3cf1e41ebcdb7e63f344a6fcabd6d241c3
ms.vergnaud@gmail.com <ms.vergnaud@gmail.com>
2023-06-15 11:34:50 +0200

    formulaire

--- frontend/src/components/compte/Compte.jsx
+++ frontend/src/components/compte/Compte.jsx
@@ -1,7 +1,33 @@
 import React from "react";
 
 function Compte() {
-  return <div />;
+  return (
+    <div className="compte">
+      <div className="connectez_vous">
+        <h2>Connectez-vous :</h2>
+
+        <form className="fomulaire_compte_connexion">
+          <input type="" placeholder="" name="" id="" required />
+          <input type="" placeholder="" name="" id="" required />
+          <button type="submit">Connection</button>
+        </form>
+      </div>
+      <div className="ou">
+        <h2>Ou</h2>
+      </div>
+      <div className="inscription">
+        <h2>Créez votre compte</h2>
+
+        <form className="fomulaire_compte_connexion">
+          <input type="" placeholder="" name="" id="" required />
+          <input type="" placeholder="" name="" id="" required />
+          <input type="" placeholder="" name="" id="" required />
+          <input type="" placeholder="" name="" id="" required />
+          <button type="submit">Inscription</button>
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export default Compte;
