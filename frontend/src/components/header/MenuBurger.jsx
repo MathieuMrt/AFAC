d80b81443a06ad376e4ab71e7bc81d105a04eef4
ms.vergnaud@gmail.com <ms.vergnaud@gmail.com>
2023-06-22 12:04:39 +0200

    menu burger avec onClick

--- frontend/src/components/header/MenuBurger.jsx
+++ frontend/src/components/header/MenuBurger.jsx
@@ -1,9 +1,22 @@
 import { NavLink } from "react-router-dom";
+import React, { useState } from "react";
 
 function MenuBurger() {
+  const [menuVisible, setMenuVisible] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuVisible(!menuVisible);
+  };
+
   return (
-    <div className="menuBurger">
-      <button className="mb_burger" type="button">
+    <div className={`menuBurger ${menuVisible ? "visible" : "hidden"}`}>
+      <button
+        className={`mb_burger ${
+          menuVisible ? "visible" : "hidden_bouton-burger"
+        }`}
+        type="button"
+        onClick={toggleMenu}
+      >
         <span className="mb_bar" />
         <span className="mb_bar" />
         <span className="mb_bar" />
@@ -14,23 +27,45 @@ function MenuBurger() {
           className="mb_header_logo"
           src="../../src/assets/img/AFAC974_Logo_600x237nega.png"
           alt="logo AFAC"
+          onClick={toggleMenu}
+          role="presentation"
         />
       </NavLink>
       <nav className="mb_navbar">
-        <NavLink className="mb_navlinks" to="/">
+        <NavLink
+          className="mb_navlinks"
+          to="/"
+          onClick={toggleMenu}
+          role="button"
+        >
           ACCUEIL
         </NavLink>
-        <NavLink className="mb_navlinks" to="/galerie">
+        <NavLink
+          className="mb_navlinks"
+          to="/galerie"
+          onClick={toggleMenu}
+          role="button"
+        >
           GALERIE
         </NavLink>
-        <NavLink className="mb_navlinks" to="/apropos">
+        <NavLink
+          className="mb_navlinks"
+          to="/apropos"
+          onClick={toggleMenu}
+          role="button"
+        >
           A PROPOS
         </NavLink>
-        <NavLink className="mb_navlinks" to="/galerie/:id/favoris">
+        <NavLink
+          className="mb_navlinks"
+          to="/galerie/:id/favoris"
+          onClick={toggleMenu}
+          role="button"
+        >
           FAVORIS
         </NavLink>
         <NavLink to="/compte">
-          <button type="button" className="mb_connexion">
+          <button type="button" className="mb_connexion" onClick={toggleMenu}>
             Connexion
           </button>
         </NavLink>
