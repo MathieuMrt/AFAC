f7d6e9a2ccb9538772e299cd34b06c1dca323191
ms.vergnaud@gmail.com <ms.vergnaud@gmail.com>
2023-07-13 11:03:59 +0200

    connexion mini menu burger fait

--- frontend/src/components/header/Header.jsx
+++ frontend/src/components/header/Header.jsx
@@ -1,9 +1,11 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { NavLink } from "react-router-dom";
 import MenuBurger from "./MenuBurger";
 import LoginContext from "../../navigation/LoginContext";
+import RouageIcon from "../../assets/img/header/rouage-icon.png";
 
 function Header() {
+  const [showSettings, setShowSettings] = useState(false);
   const { user, setUser, isConnected, setIsConnected } =
     useContext(LoginContext);
 
@@ -17,8 +19,65 @@ function Header() {
       prenom: "",
     });
     setIsConnected(false);
+    setShowSettings(false);
     localStorage.removeItem("token");
   };
+
+  const toggleParametresOpen = () => {
+    setShowSettings(!showSettings);
+  };
+
+  function connect() {
+    if (!isConnected) {
+      return (
+        <NavLink to="/compte">
+          <button type="button" className="connexion">
+            Connexion
+          </button>
+        </NavLink>
+      );
+    }
+    if (!showSettings) {
+      return (
+        <div className="connexionOn">
+          <button
+            type="button"
+            className="buttonrouage"
+            onClick={toggleParametresOpen}
+          >
+            <img src={RouageIcon} alt="Rouage" />
+          </button>
+          <div className="prenomUtilisateurConnexion">{user.prenom}</div>
+        </div>
+      );
+    }
+    return (
+      <div className="connexionOn_open">
+        <button
+          type="button"
+          className="buttonrouage"
+          onClick={toggleParametresOpen}
+        >
+          <img src={RouageIcon} alt="Rouage" />
+        </button>
+        <div className="connectionOn_open_div">
+          <div className="prenomUtilisateurConnexionOpen">{user.prenom}</div>
+          <NavLink className="linkProfilConnexion" to="/utilisateur">
+            <h5>Profil</h5>
+          </NavLink>
+          <h5
+            className="boutonDeco"
+            role="presentation"
+            onClick={handleDisconnect}
+          >
+            <NavLink className="linkProfilConnexion" to="/compte">
+              Déconnexion
+            </NavLink>
+          </h5>
+        </div>
+      </div>
+    );
+  }
 
   return (
     <div className="header">
@@ -40,21 +99,7 @@ function Header() {
       <div className="hexagone">
         <div className="hexagonemain" />
       </div>
-      <NavLink to="/compte">
-        {!isConnected ? (
-          <button type="button" className="connexion">
-            Connexion
-          </button>
-        ) : (
-          <button
-            type="button"
-            className="connexion"
-            onClick={handleDisconnect}
-          >
-            Déconnexion
-          </button>
-        )}
-      </NavLink>
+      {connect()}
       <nav className="hiddenNavPrinc">
         <NavLink
           to="/"
